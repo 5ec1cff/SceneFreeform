c09f0d374fa678ba8332bd095d6fc0e0179cc420
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2022-08-08 18:29:44 +0800

    NotificationHook: hook StatusBarNotificationActivityStarter#onNotificationClicked instead of check stack trace
    Signed-off-by: 5ec1cff <ewtqyqyewtqyqy@gmail.com>

--- app/src/main/java/five/ec1cff/scene_freeform/hook/SystemUIHooker.kt
+++ app/src/main/java/five/ec1cff/scene_freeform/hook/SystemUIHooker.kt
@@ -8,6 +8,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.service.notification.StatusBarNotification
 import com.highcapable.yukihookapi.YukiHookAPI
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
@@ -20,22 +21,19 @@ import com.highcapable.yukihookapi.hook.param.PackageParam
 import com.highcapable.yukihookapi.hook.type.android.*
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 import five.ec1cff.scene_freeform.*
+import java.util.*
 
 object SystemUIHooker: YukiBaseHooker() {
     private var mEnabled = false
     private val systemContext by lazy { getSystemContext() }
     private val activityManager by lazy { systemContext.getSystemService(ActivityManager::class.java) }
+    private val mFreeformPendingIntent = WeakHashMap<PendingIntent, Boolean>()
     private val whiteList = listOf(
         "com.termux",
         "org.telegram.messenger",
         "com.tencent.mobileqq",
         "com.tencent.mm"
     )
-
-    private fun checkFromNotification(): Boolean {
-        val stackTrace = Exception().stackTrace
-        return stackTrace.find { it.className.contains("StatusBarNotificationActivityStarter") } != null
-    }
 
     private fun updateState(key: String, newValue: Boolean? = null) {
         mEnabled = newValue ?: prefs.getBoolean(key, false)
@@ -48,6 +46,29 @@ object SystemUIHooker: YukiBaseHooker() {
             updateState(key, it)
         }
         updateState(key)
+        "com.android.systemui.statusbar.phone.StatusBarNotificationActivityStarter".hook {
+            injectMember {
+                method {
+                    name = "onNotificationClicked"
+                    param(
+                        StatusBarNotificationClass,
+                        findClass("com.android.systemui.statusbar.notification.row.ExpandableNotificationRow").normalClass!!
+                    )
+                }
+                beforeHook {
+                    val statusBarNotification = args[0] as StatusBarNotification
+                    val notification = statusBarNotification.notification
+                    loggerD(msg = "check notification statusBarNotification=$statusBarNotification notification=$notification")
+                    val intent = notification.contentIntent ?: notification.fullScreenIntent ?: return@beforeHook
+                    if (statusBarNotification.packageName in whiteList) {
+                        synchronized(mFreeformPendingIntent) {
+                            mFreeformPendingIntent[intent] = true
+                        }
+                        loggerW(msg = "mark $intent as freeform")
+                    }
+                }
+            }
+        }
         PendingIntentClass.hook {
             injectMember {
                 method {
@@ -64,11 +85,12 @@ object SystemUIHooker: YukiBaseHooker() {
                 }
                 beforeHook {
                     if (!mEnabled) return@beforeHook
-                    if (!checkFromNotification()) return@beforeHook
-                    val creatorPackage = (instance as PendingIntent).creatorPackage
-                    loggerD(msg = "notification pendingIntent for $creatorPackage")
-                    if (creatorPackage !in whiteList) return@beforeHook
+                    val intent = instance as PendingIntent
+                    synchronized(mFreeformPendingIntent) {
+                        if (mFreeformPendingIntent[intent] != true) return@beforeHook
+                    }
                     if (activityManager.getRunningTasks(1).firstOrNull()?.isHome() == true) return@beforeHook
+                    loggerD(msg = "inject freeform options for $intent")
                     args(6).set(injectFreeFormOptions(args[6] as? Bundle?))
                 }
             }
